af1fd7368161bb5e88642a521b868bc1bde17f32
daltonoswald <daltonoswald@gmail.com>
2023-06-19 12:40:10 -0400

    fixed many errors and added styles

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@ let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 let currentOperation = null
+
+if (isNaN(firstNumber)) number = 0;
+
 const display = document.querySelector('#display');
 const numberBtn = document.querySelectorAll('[data-number]');
 const operatorBtn = document.querySelectorAll('[data-operator]');
@@ -27,35 +30,41 @@ button.addEventListener('click', () => setOperator(button.textContent))
 );
 
 function decimalPoint() {
+    if (display.textContent === '') 
+    display.textContent = '0';
     if (display.textContent.includes('.')) return;
     display.textContent += '.';
 }
 
 function setOperator(operator) {
     if (currentOperation !== null) evaluate();
+    if (firstNumber === NaN) clear();
     firstNumber = display.textContent;
+    if (firstNumber === '') firstNumber = 0;
     currentOperation = operator;
     lastDisplay.textContent = `${firstNumber} ${currentOperation} `;
     display.textContent = "";
 }
 
 function evaluate() {
-    if (currentOperation === '/' && display.textContent === '0') {
+    if (currentOperation === '/' && display.textContent === '0' || display.textContent === '0.') {
         alert("You cannot divide by 0! Clearing calculator!");
         clear();
         return;
     }
     if (currentOperation === null) return;
     secondNumber = display.textContent
+    if (display.textContent === "") return;
     display.textContent = rounding(operate(firstNumber, currentOperation, secondNumber));
     lastDisplay.textContent = `${firstNumber} ${currentOperation} ${secondNumber} =`
+    currentOperation = null;
 }
 
 function clear() {
     firstNumber = "";
     secondNumber = "";
     operator = "";
-    display.textContent = "0"
+    display.textContent = ""
     lastDisplay.textContent = ""
     currentOperation = null
 };
